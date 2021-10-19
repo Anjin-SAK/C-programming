4e0d005dd0ec122c16b2a25a5552e5c4570f236d
Anjin-SAK <anjinkumar2129@gmail.com>
2021-10-19 12:54:55 +0530

    .

--- sum and differece of two digit number.c
+++ sum and differece of two digit number.c
@@ -3,12 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main()
-{
-    int i, j;
-    float f, g;
-    scanf("%d %d %f %f", &i, &j, &f, &g);
-    printf("%d %d\n%.1f %.1f", i+j, i-j, f+g, f-g);
-    
-    return 0;
+int main(){
+	int i,j;
+	float f,g;
+	scanf("%d %d %f", &i, &j, &f, &g);
+	printf("%d %D\n%.1f %f", i+j, i-j, f+g, f-g);
 }
